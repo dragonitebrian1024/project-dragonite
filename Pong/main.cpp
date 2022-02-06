9456857987f768171bbcf7a312ed67da28e72e73
ArchBacon <c.delicaat@hotmail.com>
2022-02-06 12:47:23 +0100

    Added libraries

---
+++ Pong/main.cpp
@@ -0,0 +1,8 @@
+#include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_ttf.h"
+
+int main(int argc, char** argv)
+{
+
+}
---
+++ premake5.lua
@@ -0,0 +1,65 @@
+workspace "Project"
+    architecture "x64"
+    configurations { "Debug", "Release" }
+    startproject "Pong"
+    
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Pong"
+    location "Pong"
+    kind "ConsoleApp"
+    language "C++"
+    staticruntime "Off"
+    
+    targetdir ("Binaries/" .. outputdir .. "/%{prj.name}")
+    objdir ("Intermediate/" .. outputdir .. "/%{prj.name}")
+    
+    files {
+        "%{prj.name}/main.cpp",
+        "%{prj.name}/Private/**.cpp",
+        "%{prj.name}/Public/**.h",
+        "%{prj.name}/Assets/**",
+    }
+    
+    includedirs {
+        "%{prj.name}/Engine",
+        "ThirdParty/SDL2/SDL2/include",
+        "ThirdParty/SDL2/SDL2_image/include",
+        "ThirdParty/SDL2/SDL2_ttf/include",
+    }
+
+    libdirs {
+        "ThirdParty/SDL2/SDL2/lib",
+        "ThirdParty/SDL2/SDL2_image/lib",
+        "ThirdParty/SDL2/SDL2_ttf/lib",
+    }
+
+    links {
+        "SDL2.lib",
+        "SDL2main.lib",
+        "SDL2_image.lib",
+        "SDL2_ttf.lib",
+    }
+
+    filter "system:windows"
+        cppdialect "C++20"
+        systemversion "latest"
+
+        postbuildcommands {
+            ("{COPY} ../ThirdParty/SDL2/lib/SDL2.dll ../Binaries/" .. outputdir .. "/Afterlife"),
+            ("{COPY} ../ThirdParty/SDL2_Image/lib/SDL2_image.dll ../Binaries/" .. outputdir .. "/Afterlife"),
+            ("{COPY} ../ThirdParty/SDL2_Image/lib/libpng16-16.dll ../Binaries/" .. outputdir .. "/Afterlife"),
+            ("{COPY} ../ThirdParty/SDL2_Image/lib/zlib1.dll ../Binaries/" .. outputdir .. "/Afterlife"),
+            ("{COPY} ../ThirdParty/SDL2_ttf/lib/SDL2_ttf.dll ../Binaries/" .. outputdir .. "/Afterlife"),
+        }
+    
+    filter "configurations:Debug"
+        defines { "AE_DEBUG" }
+        runtime "Debug"
+        symbols "On"
+        
+    filter "configurations:Release"
+        defines { "AE_RELEASE" }
+        runtime "Release"
+        optimize "On"
+        
