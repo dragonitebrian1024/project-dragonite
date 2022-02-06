4812dda40c6b7c33a55d9766d816b75d137c4103
ArchBacon <c.delicaat@hotmail.com>
2022-02-06 13:54:22 +0100

    Added dragonite's files

---
+++ Pong/Public/Game.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+
+#include "SDL.h"
+#include "SDL_image.h"
+
+#define SDL_MAIN_HANDLED
+
+class Game {
+
+public:
+    Game();
+
+    ~Game();
+
+    void init(std::string title, int xpos, int ypos, int width, int height, bool fullscreen);
+    void update();
+    void render();
+    void clean();
+    void handleEvents();
+
+    bool running() { return isRunning; }
+
+private:
+    bool isRunning;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+
+};
--- Pong/main.cpp
+++ Pong/main.cpp
@@ -1,8 +1,27 @@
-#include "SDL.h"
-#include "SDL_image.h"
-#include "SDL_ttf.h"
+#include <string.h>
+#include <iostream>
 
-int main(int argc, char** argv)
+#include "SDL.h"
+#include "Game.h"
+
+#undef main
+
+Game* game = nullptr;
+
+int main(int args, char* argv[])
 {
+    game = new Game();
+
+    game->init("random_pong", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+
+    while (game->running())
+    {
+        game->handleEvents();
+        game->update();
+        game->render();
+    }
+
+    game->clean(); 
+
     return 0;
 }
